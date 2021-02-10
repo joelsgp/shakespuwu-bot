563e18a2c75e64cb7e4f43897b9a43d4af7c4019
JMcB#7918 <joel.mcbride1@live.com>
2021-02-10 11:14:22 +0000

    Fix the translation chunk length

--- bot.py
+++ bot.py
@@ -10,14 +10,10 @@ CHUNKS_FILE_PATH = 'shakespuwu.json'
 def blob_to_owo_list(in_file_path='shakespeare.txt',
                      out_file_path=CHUNKS_FILE_PATH,
                      interval=TWEET_CHAR_LIMIT):
-    text_chunks = []
     with open(in_file_path, encoding='utf-8') as in_file:
-        while True:
-            read_text = in_file.read(interval)
-            if not read_text:
-                break
-            else:
-                text_chunks.append(read_text)
+        read_text = in_file.read()
+
+    text_chunks = [read_text[i:i+interval] for i in range(0, len(read_text), interval)]
 
     text_chunks = [owoify(t) for t in text_chunks]
 
